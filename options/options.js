ea7baf4399a7de3a33c83f7a717fd30f0f8400ae
Crowdbotics <team@crowdbotics.com>
2023-01-22 02:52:52 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "uvita-38388",
+  url: "https://uvita-38388.botics.co",
+  api: "https://uvita-38388.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
